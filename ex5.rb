8b4201dbdb0dedb20dadd5e73555c34e02005f23
BCRyman <ryan.stevenson@students.plymouth.ac.uk>
2016-01-18 16:48:35 +0000

    Exercises 4 and 5
    Exercises 4 and 5, that look at variables and math operators on those
    variables. Strings, Floats and Ints thus far!

---
+++ ex5.rb
@@ -0,0 +1,19 @@
+name = 'R J Stevenson'
+age = 22  #True to 2016
+height = 70 #In inches
+weight = 189 #In pounds, bit chubby at the moment, this was written after
+height_cm = height * 2.54
+weight_kg = weight * 0.45
+#Christmas
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches (that's #{height_cm} in cm) tall."
+puts "He weighs #{weight} pounds (that's #{weight_kg} in kg)."
+puts "Bit chubby in the belly, post Christmas woes."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "He has sparking #{teeth} teeth."
+
+puts "If I add #{age}, #{height} and #{weight}, I get #{age + height + weight}."
