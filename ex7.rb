3b906d05426c67e1c09c7716da64a1294bc51561
BCRyman <ryan.stevenson@students.plymouth.ac.uk>
2016-01-19 14:52:59 +0000

    Exercises 6 to 10

---
+++ ex7.rb
@@ -0,0 +1,20 @@
+puts "Mary had a little lamb."
+puts "It ran into a #{'pylon'}."
+puts "10000 volts went up it's bum."
+puts "." * 10 #copies and concatinates the string
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
