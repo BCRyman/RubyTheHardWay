655cf9adc4aa6b4d20dc3aef628bb03539f33df1
BCRyman <ryan.stevenson@students.plymouth.ac.uk>
2016-01-20 22:04:06 +0000

    Exercises 10 to 13, 13 isn't running correctly atm

---
+++ ex13.rb
@@ -0,0 +1,10 @@
+first, second, third, fourth = ARGV
+
+
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
+# "Your fourth variable is #{fourth}"
+print "How's the fourth variable?"
+fourth = gets.chomp
